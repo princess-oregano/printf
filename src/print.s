2f804aab3ee90fa06971dfa1ab341f2b3a6e0fb5
Anastasia <kulevich05@gmail.com>
2023-04-09 01:43:28 +0300

    Add comments.

--- src/print.s
+++ src/print.s
@@ -6,6 +6,8 @@
         global print
 
 ; -----------------------------------------------
+; DESCRIPTION   | General print() function.
+; -----------------------------------------------
 ; ENTRY         | RDI - pointer to format line
 ; -----------------------------------------------
 ; RETURN        | RAX - number of chars written
@@ -13,23 +15,32 @@
 ; DESTROYS      | RCX - counts number of chars
 ; -----------------------------------------------
 print:         
-        xor rcx, rcx
+        xor rcx, rcx            ; RCX = 0
 next:
-        push rdi
+        push rdi                ; Save registers.
         push rcx
-        call print_sym
+        call print_sym          ; Print next symbol in line.
         pop rcx
         pop rdi
 
-        inc rdi
-        inc rcx
+        inc rdi                 ; Move to next symbol.
+        inc rcx                 ; char_counter++
 
-        cmp byte [rdi], 0
-        jne next
+        cmp byte [rdi], 0       ; If not null-terminating byte, 
+        jne next                ; then continue.
 
-        mov rax, rcx
+        mov rax, rcx            ; Move return value to RAX.
         ret
 
+; -----------------------------------------------
+; DESCRIPTION   | Prints ASCII symbol(char).
+; -----------------------------------------------
+; ENTRY         | [IN STACK] 1: Pointer to char
+; -----------------------------------------------
+; RETURN        | NONE
+; -----------------------------------------------
+; DESTROYS      | RAX, RDI, RSI, RDX - for syscall
+; -----------------------------------------------
 print_sym:
         mov rax, 1
         mov rdi, 1
